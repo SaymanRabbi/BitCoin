ee4a720b81c404c976cf334835297a5c83a04173
sayman <mdrabbi.asm@gmail.com>
2022-08-15 20:59:00 +0600

    Menu-Bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import {Layout, Typography,Space} from 'antd';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './Components'
+import './App.css'
 
 const App = () => {
     return (
         <div className='app'>
             <div className="navbar">
-                
+                <Navbar/>
             </div>
             <div className="main">
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined,BulbOutlined, FundOutlined, MenuOutlined,MoneyCollectOutlined } from '@ant-design/icons';
+import icon from '../Images/cryptocurrency.png';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='nav-container'>
+         {/* ---------------Logo Container---------- */}
+        
+            <div className="logo-container">
+                <Avatar src={icon} size="Large"/>
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">BitCoin</Link>
+                </Typography.Title>
+                {/* <Button className='menu-control-container'></Button> */}
+            </div>
+             {/* ---------------Logo Container--------- */}
+            {/*-------------- Menu ------------------*/}
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to='/'>Home</Link>
+                </Menu.Item>
+                <Menu.Item  icon={<FundOutlined />}>
+                    <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item  icon={<MoneyCollectOutlined />}>
+                    <Link to='/exchanges'>Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to='/news'>News</Link>
+                </Menu.Item>
+                
+            </Menu>
+            {/*-------------- Menu ------------------*/}
         </div>
     );
 };
