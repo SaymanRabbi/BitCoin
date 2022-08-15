810646ef9385684d43949c33fba96dcdfe216574
sayman <mdrabbi.asm@gmail.com>
2022-08-15 17:37:21 +0600

    install dependencis

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import {Layout, Typography,Space} from 'antd';
 
 const App = () => {
     return (
-        <div>
-            hello
+        <div className='app'>
+            <div className="navbar">
+                
+            </div>
+            <div className="main">
+
+            </div>
+            <div className="footer">
+                
+            </div>
         </div>
     );
 };
